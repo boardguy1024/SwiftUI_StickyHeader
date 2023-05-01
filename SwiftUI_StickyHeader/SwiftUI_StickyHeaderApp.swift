924145fc18bd322b8d76e62ff3c339670376ead3
パク <ptn-kyungsuk.park@zozo.com>
2023-05-02 08:33:41 +0900

    Initial Commit

---
+++ SwiftUI_StickyHeader/SwiftUI_StickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StickyHeaderApp.swift
+//  SwiftUI_StickyHeader
+//
+//  Created by パク on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
